d0758525256e49f83545f58df8fefae676807ff4
Kevin Faustino <kevin@remarkablelabs.com>
2015-11-19 16:39:29 -0500

    Only log that a push notification was received when clicked

--- src/ios/PushbotsDelegate.m
+++ src/ios/PushbotsDelegate.m
@@ -7,16 +7,11 @@
     // This method will be called everytime you open the app
     // Register the deviceToken on Pushbots
     [[Pushbots sharedInstance] registerOnPushbots:deviceToken];
-	NSLog(@"Registered token is %@", deviceToken);
+  NSLog(@"Registered token is %@", deviceToken);
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
-	//Handle notification when the user click it while app is running in background or foreground.
-	if ( application.applicationState == UIApplicationStateActive ) {
-		NSLog(@"app is in foreground");
-	} else {
-		[[Pushbots sharedInstance] receivedPush:userInfo];
-	}
+  NSLog(@"push notification received");
 }
 
 @end
